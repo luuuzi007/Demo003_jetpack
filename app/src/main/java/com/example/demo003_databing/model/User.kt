9ba16a199a84b51b1a6836ebee7b7f9cc98d9453
luuuzi <1143303231@qq.com>
2020-12-29 17:55:33 +0800

    使用ktx

--- app/src/main/java/com/example/demo003_databing/model/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.demo003_databing.model
-
-/**
- * @author: Luuuzi
- * @date: 2020-11-27
- * @description:
- */
-data class User(var name:String,var password:String)
---
+++ app/src/main/java/com/example/demo003_jetpack/Test10Activity.kt
@@ -0,0 +1,26 @@
+package com.example.demo003_jetpack
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.collection.arraySetOf
+
+class Test10Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val linkedHashSet = LinkedHashSet<Int>()
+        linkedHashSet.add(1)
+        linkedHashSet.add(2)
+        linkedHashSet.add(3)
+        linkedHashSet.add(4)
+        val linkedHashSet1 = LinkedHashSet<Int>()
+        linkedHashSet1.add(3)
+        linkedHashSet1.add(4)
+        linkedHashSet1.add(5)
+        linkedHashSet1.add(7)
+        var linkedHashSet2 = LinkedHashSet<Int>()
+        linkedHashSet2.addAll(linkedHashSet)
+        linkedHashSet2.addAll(linkedHashSet1)
+        linkedHashSet2 =
+            (arraySetOf<Int>(1, 2, 3) + arraySetOf<Int>(3, 4, 5, 6, 7)) as LinkedHashSet<Int>
+    }
+}
